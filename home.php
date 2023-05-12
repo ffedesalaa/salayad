4b3f509c1068b31cacd2abef5dc18cc8e769bbde
BuildTools <unconfigured@null.spigotmc.org>
2023-05-12 09:58:55 +0200

    Home e stile

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social Network</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <a href=""><img src="" alt="logo"></a>
+        <nav>
+            <a href=""><div class="tendina" id="suggeriti">Suggeriti</div></a>
+            <a href=""><div class="tendina" id="seguiti">Seguiti</div></a>
+        </nav>
+
+
+        <a href=""><div class="login">Login</div></a>
+    </header>
+</body>
+</html>
