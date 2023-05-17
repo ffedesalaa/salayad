413c6b3f0925a63e01218464238976308b33e093
BuildTools <unconfigured@null.spigotmc.org>
2023-05-17 10:01:37 +0200

    login registrazione  salvataggio dati del database

--- File_PHP/signin.php
+++ File_PHP/signin.php
@@ -1,9 +1,10 @@
 <?php
-  if(isset($_POST['nome'])) $nome = $_POST['nome'];  else $nome = "";     
-  if(isset($_POST['cognome'])) $cognome = $_POST['cognome'];  else $cognome = "";     
-  if(isset($_POST['email'])) $email = $_POST['email'];  else $email = "";     
-  if(isset($_POST['password'])) $password = $_POST['password'];  else $password = "";     
-  if(isset($_POST['numero'])) $numero= $_POST['numero'];  else $numero = "";     
+    if(isset($_POST['nome'])) $nome = $_POST['nome'];  else $nome = "";     
+    if(isset($_POST['cognome'])) $cognome = $_POST['cognome'];  else $cognome = "";     
+    if(isset($_POST['username'])) $username = $_POST['username']; else $username = "";
+    if(isset($_POST['email'])) $email = $_POST['email'];  else $email = "";     
+    if(isset($_POST['password'])) $password = $_POST['password'];  else $password = "";     
+    if(isset($_POST['numero'])) $numero= $_POST['numero'];  else $numero = "";  
 ?>
 
 
@@ -17,6 +18,7 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
+    
     
     <header>
         <a href="../home.php"><div class="logo"><b>MOMENTUM</b></div></a>
@@ -35,7 +37,11 @@
                     <input type="text" name="cognome" id="cognome" placeholder="Inserire il cognome" onclick="changePlaceholder()">
                 </div>
                 <div class="input-box">
-                    <label for="email">Email</label>
+                    <label for="username" id="labelUsername">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Inserire lo username" onclick="changePlaceholder()">
+                </div>
+                <div class="input-box">
+                    <label for="email" id="labelEmail">Email</label>
                     <input type="text" name="email" id="email" placeholder="Inserire l'email" onclick="changePlaceholder()">
                 </div>
                 <div class="input-box">
@@ -67,35 +73,75 @@
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
     <?php 
-        if(isset($_POST['nome']) AND isset($_POST['cognome']) AND isset($_POST['email']) AND isset($_POST['password']) ){
+        if(isset($_POST['nome']) AND isset($_POST['cognome']) AND isset($_POST['username']) AND isset($_POST['email']) AND isset($_POST['password']) ){
 
             $conn = new mysqli("localhost","root", "", "momentum");
 
-            if (!$conn) {
-                die('Connesione al database fallita :' . mysqli_connect_error());
+            if ($conn->connect_error) {
+                die('Connesione al database fallita :' . $conn->connect_error);
             }else{
-                $myquery = "SELECT email
+                 
+
+                $myquery1 = "SELECT email
                         FROM utente
                         WHERE email = '".$email. "'";
-
-                $ris = $conn->query($myquery);
+                $ris1 = $conn->query($myquery1);
               
-                if($ris->num_rows > 0){
-                 
-                }else{
-                    $myquery = "INSERT INTO utente (Nome, Cognome, Email, Password, Numero )
-                                VALUES ('$nome', '$cognome', '$email', '$password', '$numero' )";
+                $myquery2 = "SELECT username
+                        FROM utente
+                        WHERE username = '".$username. "'";
+                $ris2 = $conn->query($myquery2);
+
+                if($ris1->num_rows > 0 || $ris2->num_rows > 0){
+                    if($ris1->num_rows > 0 AND $ris2->num_rows > 0){
+                        die("        
+                            <script>
+                                var labelEmail = document.getElementById('labelEmail');
+                                labelEmail.innerHTML='Email già in uso';
+                                labelEmail.style.color = 'red';
+
+                                var labelUser = document.getElementById('labelUsername');
+                                labelUser.innerHTML='Username già in uso';
+                                labelUser.style.color = 'red';
+                            </script>");
+
+                    }elseif($ris2->num_rows > 0){
+                        die("        
+                        <script>
+                            var labelUser = document.getElementById('labelUsername');
+                            labelUser.innerHTML='Username già in uso';
+                            labelUser.style.color = 'red';
+                        </script>");
+                        
+                    }
+                    elseif($ris1->num_rows > 0){
+                        die("        
+                            <script>
+                                var labelEmail = document.getElementById('labelEmail');
+                                labelEmail.innerHTML='Email già in uso';
+                                labelEmail.style.color = 'red';
+                            </script>");
+                    }
+                }
+                else{
+                    $myquery = "INSERT INTO utente (Nome, Cognome, Username, Email, Password, Numero )
+                                VALUES ('$nome', '$cognome', '$username','$email', '$password', '$numero' )";
                 
-        
+                    
                     if($conn->query($myquery) === true){
                         session_start();
                         $_SESSION['nome'] = $nome;
                         $_SESSION['cognome'] = $cognome;
+                        $_SESSION['username'] = $username;
                         $_SESSION['email'] = $email;
                         $_SESSION['password'] = $password;
                         $_SESSION['numero'] = $numero;
+                        $_SESSION['accesso'] = true;
 
                         $conn->close();
+                        header('Refresh: 5; URL=../home.php');
+
+
                     }
                 }
             }
@@ -105,7 +151,5 @@
     <footer>
         <div>Sito fatto dai Salayad</div>
     </footer>
-
-
 </body>
 </html>
